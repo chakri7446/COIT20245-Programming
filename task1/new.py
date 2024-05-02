f7a26207e61135421317bbe02cf84a6e70866688
chakri7446 <163386052+chakri7446@users.noreply.github.com>
2024-05-02 15:54:03 +1000

    Update new.py

---
+++ task1/new.py
@@ -0,0 +1,9 @@
+def display_menu():
+    print("Help")
+    print("===")
+    print("The following commands are recognised")
+    #Formatiing the menu to show align
+    print('Display help{:>20} wildlife> help'.format(''))
+    print('Exit the appliation {:>20} wildlife> exit'.format(''))
+
+ display_menu();   
