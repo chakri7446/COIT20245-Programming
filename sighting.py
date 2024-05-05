7e027efd4a4764af5502262a38c24cf7397585aa
chakri7446 <163386052+chakri7446@users.noreply.github.com>
2024-05-05 22:20:34 +1000

    Update sighting.py

--- sighting.py
+++ sighting.py
@@ -1,11 +1,36 @@
-Task-1
-
+ display_menu();   
 def display_menu():
     print("Help")
     print("===")
-    print("The following commands are recognised")
+    print("The following commands are recognised.")
     #Formatiing the menu to show align
-    print('Display help{:>20} wildlife> help'.format(''))
-    print('Exit the appliation {:>20} wildlife> exit'.format(''))
+    print('Display help {:>20}  wildlife> help'.format(''))
+    print('Display animal species in a city   wildlife> species Cairns')
+    print('Display venonmous species wildlife> species Cairns venomous')
+    print('Display animal sightings in a city wildlife> sightings Cairns 1039')
+    print('Exit the application {:>12}  wildlife> exit\n'.format(''))
 
- display_menu();   
+
+def main():
+    display_menu()
+    while(True):
+        comm = input().split(' ')
+        print("\n")
+        if(comm[0]=="exit"):
+            exit(0)
+        elif(comm[0]=="help"):
+            display_menu()
+        elif(comm[0]=="species"):
+            
+            if len(comm)>2:
+                display_species(filter_venomous(search_species(comm[1])))
+            else:
+                display_species(search_species(comm[1]))
+            
+        elif(comm[0]=="sightings"):
+            display_sightings(search_sightings(comm[2],comm[1]))
+        else:
+            print("ERROR: Unrecognized Command. Please check and try again.")
+            display_menu()
+
+def search_species(city):
